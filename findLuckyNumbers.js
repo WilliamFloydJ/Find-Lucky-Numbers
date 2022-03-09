051172ef67e6a8ab2d0fc27fb97b12ab53c790d7
William <Floydfamily71@gmail.com>
2022-03-08 19:20:27 -0700

    Working Iteration

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,17 @@
 // Write your code below this line.
+const luckyNumber = (num) => {
+  let RanNums = [];
+  if (10 > num > 0) {
+    for (let i = 0; i < num; i++) {
+      let RanNumber = Math.floor(Math.random() * 10 + 1);
+      if (RanNums.includes(RanNumber) === false) {
+        RanNums.push(RanNumber);
+      } else {
+        i--;
+      }
+    }
+    return RanNums;
+  }
+};
+
+console.log(luckyNumber(9));
